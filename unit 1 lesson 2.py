8f7e3fee23ea84b836f8ca63495b207ca468a60d
Shayan762655 <762655@pdsb.net>
2025-02-08 18:31:43 -0500

    Create unit 1 lesson 2.py

---
+++ unit 1 lesson 2.py
@@ -0,0 +1,13 @@
+# predict:
+# The output will be 24
+# Run: 
+# My prediction was correct and the output was 24
+# Modify 
+amount = 24
+print("Two dozen is: ", amount)
+# Using the variable amount can help with readability of the code and printing "Two dozen is: " gives back more feedback and infomration whenrunning it
+amount = 7 * 4
+print("forty-two is: ", amount) 
+# The fixed output would be
+amount = 7 * 4
+print("twenty-eight is: ", amount)
