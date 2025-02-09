47a6684702dcaba0db661100ebd167fe52da0a50
Shayan762655 <762655@pdsb.net>
2025-02-09 16:12:10 -0500

    unit 1 lesson 3.py

---
+++ unit 1 lesson 3.py
@@ -0,0 +1,20 @@
+# Task A
+# The output will be:
+# Welcome to Sanjay's Koala Party!
+# It's possible Sanjay can distribute  12
+#  cookies for each bowl.
+
+# my prediction was correct
+# fix the program so that there is one space before and after "12":
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute", 12)
+print("cookies for each bowl.")
+
+#task B:
+print("Sanjay has", 6, "bowls for", 144, "cookies")
+# you can also do
+print("Sanjay has %d bowls for %d cookies" % (6,144))
+
+#task C:
+print("The number of cookies per bowl is:", 144/6)
+#there is one text value and one number value (two numbers being divided with the / operator)
