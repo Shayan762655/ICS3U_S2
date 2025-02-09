5105888d9b6a655057b7905a07f05ee7327fc0f8
Shayan762655 <762655@pdsb.net>
2025-02-09 15:53:27 -0500

    unit 1 lesson 2.py

--- unit 1 lesson 2.py
+++ unit 1 lesson 2.py
@@ -11,3 +11,24 @@ print("forty-two is: ", amount)
 # The fixed output would be
 amount = 7 * 4
 print("twenty-eight is: ", amount)
+# or
+amount = 7 * 6
+print("forty-two is: ",amount)
+# modify 3
+amount = 8
+amount = amount + 1
+# or
+amount = += 1
+print("the amount is: ",amount)
+# this is telling us that the amount, which is 8 is being added to 1, which gives us 9
+# it can also be written as += 1
+# += 1 works the same way and adds 1 to the value of amount 
+# modify 4
+amount = 8
+amount = amount * 2
+# or
+amount *= 2
+print(amount)
+# this is telling us that the amount, which is 8 is being mutlipled into 2, which gives us 16
+# it can also be written as *= 2
+# *= 2 works the same way and mulples 2 to the value of amount
