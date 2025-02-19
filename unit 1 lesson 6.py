35a46c0f52d9ceb72f8db38676678baedb6fe0de
Shayan762655 <762655@pdsb.net>
2025-02-18 21:30:43 -0500

    Update unit 1 lesson 6.py

--- unit 1 lesson 6.py
+++ unit 1 lesson 6.py
@@ -1,1 +1,7 @@
 birth_year = int(input("Please enter your year of birth: "))
+age = int(input("please enter your age: "))
+Result = Birth_year * 2 + 5
+Result = Result * 50 + age 
+Result = Result - 250 
+Result = Result / 100
+Print("your birth year with the decimal of your age is:", result) 
